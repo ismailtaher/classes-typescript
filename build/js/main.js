d63f5452dffe2ab79899ac79f8ff7546268aeaf2
ismailtaher <ismail.taher@ymail.com>
2025-05-07 19:55:10 +0500

    Add visibility modifiers & methods to access public & private members

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,17 @@
 "use strict";
 class coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = "TypeScript" // Making this param optional and adding default value of "TypeScript"
+    ) {
         this.name = name;
         this.music = music;
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello I'm ${this.age}`;
+    }
 }
+const Ismail = new coder("Ismail", "Indie", 26);
+console.log(Ismail.getAge());
+/* console.log(Ismail.age);
+console.log(Ismail.lang); */
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 class coder {
-  secondLang!: string;
+  secondLang!: string; // Assertion on secondLang with "!" to not have it initialize right away
 
   constructor(
     public readonly name: string,
     public music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = "TypeScript" // Making this param optional and adding default value of "TypeScript"
   ) {}
+
+  public getAge() {
+    return `Hello I'm ${this.age}`;
+  }
 }
+
+const Ismail = new coder("Ismail", "Indie", 26);
+console.log(Ismail.getAge());
+/* console.log(Ismail.age);
+console.log(Ismail.lang); */
